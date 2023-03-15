2080ba9d9cf4c9ae5766e9fc98d8586c075d4c5f
Mathieu Antunes <mathieu.antunes31@gmail.com>
2023-03-15 16:09:34 +0100

    Fix code pas beau

--- php/index.php
+++ php/index.php
@@ -1,32 +1,11 @@
-<?php
+<?php //fichier principal de l'application
 session_start();
-?>
-<!DOCTYPE HTML>
-<html lang="fr" style="font-family: Arial,sans-serif;">
-
-<head>
-    <meta charset="utf-8">
-    <!-- importer le fichier de style -->
-    <link rel="stylesheet" href="style/style.css" media="screen" type="text/css" />
-    <title>bienvenue</title>
-</head>
-
-<body>
-    <?php
 
 if(isset($_SESSION['token'])){
     echo"<p>connecté</p>";
 }else{
     echo'<a href="login.php">Connexion</a>';
 }
-?>
-
-</body>
-
-</html>
-
-<?php //fichier principal de l'application
-
 ?>
 <!DOCTYPE html>
 <html lang="fr">
